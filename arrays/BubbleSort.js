caa3b0b7f5ec28a8f47d7b7f5e7fb427db063351
Cheikh Gueye Wane <cheikhgueye.wane@gmail.com>
2022-01-21 20:49:36 +0000

    add insert and selection sort

--- arrays/BubbleSort.js
+++ arrays/BubbleSort.js
@@ -1,26 +1,27 @@
 /**
- * Selection sort algorithm (by ascend order)
+ * Bubble sort algorithm
  * Complexity :
  *  - Worst case O (n^2)
  *  - Best case  O (n)
  * @param {Array} A array to sort
  */
-function selectSort(A) {
+function bubbleSort(A) {
   if (A.length === 0) return A;
-
-  for (let i = 0; i < A.length - 1; i++) {
-    let index = i;
-    for (let j = i + 1; j < A.length; j++) {
-      index = A[j] < A[index] ? j : index;
+  for (let i = 0; i < A.length; i++) {
+    const hasSwapped = false;
+    for (let j = 0; j < A.length - 1; j++) {
+      if (A[j] > A[j + 1]) {
+        hasSwapped = true;
+        [(A[i], A[index])] = [A[index], A[i]];
+      }
     }
-    [A[i], A[index]] = [A[index], A[i]];
-  }
 
-  return A;
+    return A;
+  }
 }
 
 console.log(
-  selectSort([
+  insertSort([
     76, 72, 54, 50, 85, 47, 59, 7, 47, 81, 60, 6, 15, 30, 32, 75, 94, 23, 64, 1,
     96, 57, 85, 40, 13, 38, 53, 67, 83, 26, 84, 15, 74, 8, 9, 76, 76, 95, 55,
     40, 32, 19, 76, 75, 30, 71, 13, 19, 52, 61,
